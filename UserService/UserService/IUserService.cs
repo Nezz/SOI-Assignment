475e176c1dc5033411146c18a3cf8968c67729b0
Nezz <bloodsource@vipmail.hu>
2014-12-06 14:30:39 +0100

    Basic UserService

---
+++ UserService/UserService/IUserService.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace UserService
+{
+    [ServiceContract]
+    public interface IUserService
+    {
+
+        [OperationContract]
+        string GetName();
+
+        [OperationContract]
+        GeoLocation GetGeoLocation(string ipAddress);
+    }
+
+    [DataContract]
+    public class GeoLocation
+    {
+
+        [DataMember]
+        public string CountryCode { get; set; }
+
+        [DataMember]
+        public string Country { get; set; }
+
+        [DataMember]
+        public string City { get; set; }
+
+        [DataMember]
+        public int Latitude { get; set; }
+
+        [DataMember]
+        public int Longitude { get; set; }
+    }
+}
---
+++ UserService/UserService/UserService.svc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace UserService
+{
+    public class UserService : IUserService
+    {
+        public string GetName()
+        {
+            return "User" + Guid.NewGuid().ToString().Substring(0, 8);
+        }
+
+        public GeoLocation GetGeoLocation(string ipAddress)
+        {
+            return this.GetGeoLocationAsync(ipAddress).Result;
+        }
+
+        private async Task<GeoLocation> GetGeoLocationAsync(string ipAddress)
+        {
+            var client = new HttpClient();
+
+            var response = await client.GetAsync("http://www.telize.com/geoip/" + ipAddress);
+            if (response.StatusCode == HttpStatusCode.Unauthorized)
+                throw new FaultException(await response.Content.ReadAsStringAsync());
+
+            return await JsonConvert.DeserializeObjectAsync<GeoLocation>(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
